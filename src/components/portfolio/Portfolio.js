c2594fef38bc8ad8f6194e75afc06c72e294f180
baxa88 <mamasolibaxromjon@gmail.com>
2022-04-25 15:23:17 +0500

    git link

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -12,42 +12,42 @@ const data=[
         id:1,
         image:IMG1,
         title:`Agro Olam o'simliklar haqida`,
-        github:'',
+        github:'https://github.com/baxa88/loyiha.git',
         demo:'https://agro-olam.netlify.app'
     },
     {
         id:2,
         image:IMG2,
         title:'Internet magazin ',
-        github:'',
+        github:'https://github.com/baxa88/Xcolor.git',
         demo:'https://xcolor-1.netlify.app'
     },
     {
         id:3,
         image:IMG3,
         title:'Youtube clone API',
-        github:'',
+        github:'https://github.com/baxa88/youtube-clone.git',
         demo:'https://youtube-clone-55.netlify.app'
     },
     {
         id:4,
         image:IMG4,
         title:'Matnni QR kodga aylantirish',
-        github:'',
+        github:'https://github.com/baxa88/QR-code.git',
         demo:'https://qr-kodni-skaner.netlify.app'
     },
     {
         id:5,
         image:IMG5,
         title:'Kino katalog API',
-        github:'',
+        github:'https://github.com/baxa88/kino-katalog.git',
         demo:'https://kino-katalog.netlify.app'
     },
     {
         id:6,
         image:IMG6,
         title:'Online chat ',
-        github:'',
+        github:'https://github.com/baxa88/realtime-chat.git',
         demo:'https://online-chatt.netlify.app'
     }
 ]
